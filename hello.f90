79dcdc9e890e2ece4da39cb23d3b3b30dbee6018
Tianli Jin <jtl1861@163.com>
2019-11-19 20:21:03 +0800

    dev3 branch commit ff

--- hello.f90
+++ hello.f90
@@ -2,3 +2,5 @@ Modify 2nd time new lines for remote origin master and need merge.
 Modify 1st time for feature 1
 
 Modify 3rd time for test --no-ff
+Modify 4th time for test ff VS--no-ff
+Modify 5th time for test ff VS--no-ff, due to 4th time failure

2196dea6cac3e4c9e4121d6b9eb0208336999494
Tianli Jin <jtl1861@163.com>
2019-11-19 20:22:54 +0800

    Modify 6th time for test ff VS--no-ff, due to 4th time failure

--- hello.f90
+++ hello.f90
@@ -4,3 +4,4 @@ Modify 1st time for feature 1
 Modify 3rd time for test --no-ff
 Modify 4th time for test ff VS--no-ff
 Modify 5th time for test ff VS--no-ff, due to 4th time failure
+Modify 6th time for test ff VS--no-ff, due to 4th time failure
